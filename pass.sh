5fc363e3563cd7814417dd3ee4edec8227dac00e
Christopher P. Bills <cpbills@fauxtographer.net>
2013-09-05 01:50:14 -0700

    Update password maker algorithm
    Move from 8 character passwords to 16 character complex passwords.
    Most sites these days will support this approach.

--- pass.sh
+++ pass.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+SEED="someseed"
 echo -n "site: "
 read site
 echo -n "pass: "
 stty -echo
 read pass
 stty echo
-echo -n "$pass$site" | md5sum | cut -c1-8
+echo
+oldpass=`echo -n "$pass$site" | md5sum | cut -c1-8`
+echo "old pass: $oldpass"
+complexity_insurance='LoL69%'
+pass=`echo -n "$pass$site" | mkpasswd -m sha-256 -s "$SEED" -s | cut -c34-44`
+echo -n $pass$complexity_insurance | xclip
+sleep 10
