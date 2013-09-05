eef470e8c392ac8708167e2c7d62b8e889185b4c
Christopher P. Bills <cpbills@fauxtographer.net>
2013-09-05 01:55:41 -0700

    Added ability to skip tracks with osd-mpd
    Skip and rewind tracks and display information on screen

--- osd-mpd.sh
+++ osd-mpd.sh
@@ -6,18 +6,47 @@ MPC=/usr/bin/mpc
 MSG='Not Handled Yet'
 
 case $1 in
-    'next')
+    next)
         MSG='Next Track'
         STATUS=`$MPC next`
         ;;
-    'prev')
+    prev)
         MSG='Previous Track'
         STATUS=`$MPC prev`
         ;;
+    toggle|pause|play)
+        STATUS=`$MPC toggle`
+        if [ $? -ne 0 ]; then
+            MSG='[mpd] error playing song'
+        else
+            if echo "$STATUS" | grep -e '^\[playing\]' > /dev/null 2>&1; then
+                MSG='welcome back'
+            else
+                MSG='pausing for now...'
+            fi
+        fi
+        ;;
     *)
-        exit
+        STATUS=`mpc`
+        if [ $? -ne 0 ]; then
+            MSG='[mpd] error getting status'
+        else
+            if [ $(echo "$STATUS" | wc -l) -eq 1 ]; then
+                MSG='the party is dead man, the music is stopped'
+            else
+                if echo "$STATUS" | grep -e '^\[playing\]' > /dev/null 2>&1; then
+                    MSG='now playing'
+                else
+                    MSG='paused and waiting'
+                fi
+            fi
+        fi
         ;;
 esac
-SONG=`echo "$STATUS"|head -n1`
-OUTPUT=`printf "%s\n%s\n" "$MSG" "$SONG"`
+if [ $(echo "$STATUS" | wc -l) -eq 1 ]; then
+    OUTPUT="$MSG"
+else
+    SONG=`echo "$STATUS"|head -n1`
+    OUTPUT=`printf "%s\n%s\n" "$MSG" "$SONG"`
+fi
 $OSD -t "$OUTPUT"
