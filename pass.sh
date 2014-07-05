8d4df756f6142a226ac994f2eae2184b88a6bfb9
Christopher P. Bills <cpbills@fauxtographer.net>
2014-07-05 03:49:44 -0400

    Add script to disable xscreensaver
    Used by mplayer to prevent the screen from locking when watching movies,
    etc. It's configured in ~/.mplayer/config

--- pass.sh
+++ pass.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
-SEED="someseed"
-echo -n "site: "
-read site
-echo -n "pass: "
+# salt must be 8+ characters
+salt='someseed'
+# appended to passwords to make sure all character classes are covered
+hit_all_reqs='LoL69%'
+# password length
+length=16
+# length of sha-256 hash is 43 chars
+hash_start=18
+hash_end=$(((hash_start + length - 1) - ${#hit_all_reqs}))
+
+read -p "site: " site
 stty -echo
-read pass
+read -p "pass: " pass
 stty echo
 echo
-oldpass=`echo -n "$pass$site" | md5sum | cut -c1-8`
-echo "old pass: $oldpass"
-complexity_insurance='LoL69%'
-pass=`echo -n "$pass$site" | mkpasswd -m sha-256 -s "$SEED" -s | cut -c34-44`
-echo -n $pass$complexity_insurance | xclip
+
+pass_id=$(printf "%s%s" "$pass" "$site" | md5sum | cut -c1-8)
+pass_hash=$(
+  printf "%s%s" "$pass" "$site" \
+  | mkpasswd -m sha-256 -S "$salt" -s \
+  | cut -c$((5 + ${#salt}))- \
+  | cut -c${hash_start}-${hash_end}
+)
+echo "pass id: $pass_id"
+printf "%s%s" "$pass_hash" "$hit_all_reqs" | xclip
 sleep 10
