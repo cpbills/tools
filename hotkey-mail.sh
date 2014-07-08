9e0ff1c0f53a78624fafae8a2754872ad74e83f1
Christopher P. Bills <cpbills@fauxtographer.net>
2014-07-08 00:53:47 -0400

    Update mail helper scripts
    grab-mail:
      - Print output, handle redirecting to /dev/null in crontab and helper
        scripts
      - Use bash
      - Use -print0 to use 'good practices', in case I decide to name config
        files idiotically.
    
    hotkey-mail.sh
      - Use bash
      - Call 'grab-mail' instead of 'grab-mail.sh'

--- hotkey-mail.sh
+++ hotkey-mail.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 HEIGHT=50
 WIDTH=80
 COMMAND='/usr/bin/mutt -F ~/.mutt/local.conf'
 
-/home/cpbills/tools/grab-mail.sh &
+/home/cpbills/tools/grab-mail &>/dev/null &
 /usr/bin/uxterm -g ${WIDTH}x${HEIGHT} -T 'mutt' -e $COMMAND
